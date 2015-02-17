f3b0042533729bf789da2fd1e9635c9a5a712930
aleksejsaleksejevs <aleksejs.aleksejevs@gmail.com>
2015-02-17 14:56:07 +0200

    made changes in body fields - added some images, text etc

--- index.php
+++ index.php
@@ -7,6 +7,24 @@
 <script src="script.js"></script>
 </head>
 <body>
-<h1>Hello</h1>
+<div id="header">
+<a href="second.php"><h1>Hello</h1></a>
+<img src="http://www.web3d.org/x3d/content/examples/HelloWorld.tall.png" />
+</div>
+<div id="body">
+<span class="test">This is just a text</span>
+</div>
+<div id="footer">
+<table>
+<tr>
+<td>1</td>
+<td>2</td>
+</td>
+<tr>
+<td>3</td>
+<td>4</td>
+</tr>
+</table>
+</div>
 </body>
 </html>
