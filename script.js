8e1e4c64d4f07b14ab352e5839b92c7e17d44cf8
aleksejsaleksejevs <aleksejs.aleksejevs@gmail.com>
2015-02-17 17:22:14 +0200

    changes in H1 fonts

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 console.log("Hello");
 
 for(var i = 1; i <= 30; i++){
-  if(i % 3 == 0){
-    if(i % 5 == 0){
+  if((i % 3 == 0) && (i % 5 == 0)){
       console.log("FizzBuzz");
-    }else{
+  }else if(i % 3 == 0){
       console.log("fizz");
-    }
-  }else if(i % 17 == 0){
-    console.log("This can be divided by 17");
   }else if(i % 5 == 0){
     console.log("buzz");
   }else{
